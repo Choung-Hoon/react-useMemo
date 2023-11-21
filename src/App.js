814fa5f2a39d7de2b335335313e494c9784a9c97
qrisma <nobel77.lee@gmail.com>
2023-11-21 09:15:13 +0900

    v1 : useMemo 사용전

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+const hardCalculate = (number) => {
+  console.log("어려운 계산");
+  for (let i = 0; i < 999999999; i++) {} // 생각하는 시간, 부하주는 코드
+  return number + 10000;
+};
+const easyCalculate = (number) => {
+  console.log("쉬운 계산");
+  return number + 1;
+};
 
 function App() {
+  const [hardNumber, setHardNumber] = useState(1);
+  const [easyNumber, setEasyNumber] = useState(1);
+
+  // 렌더링 마다 아래 함수가 실행되어 부하 발생
+  // 쉬운 계산기 값만 바꿔도 어려운 계산기 함수가 동작한다.
+  const hardSum = hardCalculate(hardNumber);
+  const easySum = easyCalculate(easyNumber);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>어려운 계산기</h3>
+      <input type="number" value={hardNumber} onChange={(e) => setHardNumber(e.target.value)} />
+      <span> + 10000 = {hardSum}</span>
+
+      <h3>쉬운 계산기</h3>
+      <input type="number" value={easyNumber} onChange={(e) => setEasyNumber(e.target.value)} />
+      <span> + 1 = {easySum}</span>
     </div>
   );
 }
