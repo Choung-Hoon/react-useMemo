bb2d5d2739a004865337eea0456f7309336d2fe8
qrisma <nobel77.lee@gmail.com>
2023-11-21 09:36:56 +0900

    v3 : useMemo를 이용하여 객체를 memoization

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
-import { useState } from "react";
-
-const hardCalculate = (number) => {
-  console.log("어려운 계산");
-  for (let i = 0; i < 999999999; i++) {} // 생각하는 시간, 부하주는 코드
-  return number + 10000;
-};
-const easyCalculate = (number) => {
-  console.log("쉬운 계산");
-  return number + 1;
-};
+import { useEffect, useMemo, useState } from "react";
 
 function App() {
-  const [hardNumber, setHardNumber] = useState(1);
-  const [easyNumber, setEasyNumber] = useState(1);
+  const [number, setNumber] = useState(0);
+  const [isKorea, setIsKorea] = useState(true);
 
-  // 렌더링 마다 아래 함수가 실행되어 부하 발생
-  // 쉬운 계산기 값만 바꿔도 어려운 계산기 함수가 동작한다.
-  const hardSum = hardCalculate(hardNumber);
-  const easySum = easyCalculate(easyNumber);
+  // const location = {
+  //   contry: isKorea ? "한국" : "외국",
+  // };
+  const location = useMemo(() => {
+    return {
+      contry: isKorea ? "한국" : "외국",
+    };
+  }, [isKorea]);
+
+  // 연관 배열이 객체
+  // 객체는 렌더링할때 마다 재할당 => useEffect가 실행됨
+  // 객체는 useMemo를 이용하여 저장해놓으면 재할당이 안되어 useEffect 실행안됨
+  useEffect(() => {
+    console.log("userEffect 호출");
+  }, [location]);
 
   return (
     <div className="App">
-      <h3>어려운 계산기</h3>
-      <input type="number" value={hardNumber} onChange={(e) => setHardNumber(e.target.value)} />
-      <span> + 10000 = {hardSum}</span>
-
-      <h3>쉬운 계산기</h3>
-      <input type="number" value={easyNumber} onChange={(e) => setEasyNumber(e.target.value)} />
-      <span> + 1 = {easySum}</span>
+      <h2>하루에 몇끼 먹어요?</h2>
+      <input type="number" value={number} onChange={(e) => setNumber(e.target.value)} />
+      <hr />
+      <h2>어느 나라에 있어여?</h2>
+      <p>나라 : {location.contry}</p>
+      <button onClick={() => setIsKorea(!isKorea)}>비행기 타자</button>
     </div>
   );
 }
